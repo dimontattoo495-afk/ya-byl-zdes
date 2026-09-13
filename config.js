cfa012ad23e796fda7c3b13fc55b7732c5c25b22
dimontattoo495-afk <dimontattoo495@gmail.com>
2026-09-13 13:40:07 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.YBZ_CONFIG = {
+  SUPABASE_URL: "NEXT_PUBLIC_SUPABASE_URL=https://qgccfpkvzrimwqmqrxwq.supabase.co",
+  SUPABASE_ANON_KEY: "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_y1H8hlMKSlLaim-9d-Qg3g_OqOJ4IYr",
+  SITE_NAME: "Я БЫЛ ЗДЕСЬ"
+};
